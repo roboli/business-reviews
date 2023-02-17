d707690cf0c83f291adfa0fe4fe431d0b34fdf2c
roboli <robertooliveros@mac.com>
2023-02-17 08:19:07 -0600

    Add neo4j

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,27 +1,129 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const neo4j = require('neo4j-driver');
+const { Neo4jGraphQL } = require('@neo4j/graphql');
+const {
+  Neo4jGraphQLAuthJWKSPlugin
+} = require("@neo4j/graphql-plugin-auth");
 
 const typeDefs = gql`
   type Query {
-    greetings(name: String = "GRANDstack"): String
+    fuzzyBusinessByName(searchString: String): [Business!]!
+      @cypher(
+        statement: """
+        CALL db.index.fulltext.queryNodes( 'businessNameIndex', $searchString+'~')
+        YIELD node RETURN node
+        """
+      )
+  }
+
+  type Business {
+    businessId: ID!
+    waitTime: Int! @customResolver
+    averageStars: Float!
+      @auth(rules: [{ isAuthenticated: true }])
+      @cypher(
+        statement: "MATCH (this)<-[:REVIEWS]-(r:Review) RETURN avg(r.stars)"
+      )
+    recommended(first: Int = 1): [Business!]!
+      @cypher(
+        statement: """
+        MATCH (this)<-[:REVIEWS]-(:Review)<-[:WROTE]-(:User)-[:WROTE]->(:Review)-[:REVIEWS]->(rec:Business)
+        WITH rec, COUNT(*) AS score
+        RETURN rec ORDER BY score DESC LIMIT $first
+        """
+      )
+    name: String!
+    city: String!
+    state: String!
+    address: String!
+    location: Point
+    reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
+    categories: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
+  }
+
+  type User {
+    userId: ID!
+    name: String!
+    reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
+  }
+
+  extend type User
+    @auth(
+      rules: [
+        { operations: [READ], where: { userId: "$jwt.sub" } }
+        { operations: [CREATE, UPDATE, DELETE], roles: ["admin"] }
+      ]
+    )
+
+  type Review {
+    reviewId: ID!
+    stars: Float!
+    date: Date
+    text: String
+    user: User! @relationship(type: "WROTE", direction: IN)
+    business: Business! @relationship(type: "REVIEWS", direction: OUT)
+  }
+
+  extend type Review
+    @auth(
+      rules: [
+        {
+          operations: [CREATE, UPDATE]
+          bind: { user: { userId: "$jwt.sub" } }
+        }
+      ]
+    )
+
+  type Category {
+    name: String!
+    businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
+    total: Float!
+      @cypher(
+        statement: "MATCH (this)<-[:IN_CATEGORY]-(b:Business) RETURN count(b)"
+      )
   }
 `;
 
 const resolvers = {
-  Query: {
-    greetings: (parent, args, context) => {
-      return `Hello, ${args.name}!`;
-    }
-  }
+  Business: {
+    waitTime: (obj, args, context, info) => {
+      var options = [0, 5, 10, 15, 30, 45];
+      return options[Math.floor(Math.random() * options.length)];
+    },
+  },
 };
 
-const server = new ApolloServer({
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
+
+const neoSchema = new Neo4jGraphQL({
   typeDefs,
-  resolvers
+  resolvers,
+  driver,
+  plugins: {
+    auth: new Neo4jGraphQLAuthJWKSPlugin({
+      jwksEndpoint: "https://dev-ohawt3ax5uu3euvw.us.auth0.com/.well-known/jwks.json",
+    }),
+  }
 });
 
-const serverHandler = server.createHandler();
+const initServer = async () => {
+  return await neoSchema.getSchema().then((schema) => {
+    const server = new ApolloServer({
+      schema,
+      context: ({ event }) => ({ req: event }),
+    });
 
-exports.handler = (event, context, cb) => {
+    const serverHandler = server.createHandler();
+    return serverHandler;
+  });
+};
+
+exports.handler = async (event, context, cb) => {
+  const serverHandler = await initServer();
+
   return serverHandler({
     ...event,
     requestContext: event.requestContext || {}
